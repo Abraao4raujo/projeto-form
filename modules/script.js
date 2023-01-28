a08f90f32ea99e0bd743505c0a697cb64e19786d
Abraão Araujo <a68816619@gmail.com>
2023-01-27 22:27:35 -0300

    projeto-form-atualizado

--- modules/script.js
+++ modules/script.js
@@ -1,11 +1,16 @@
 var btn = document.querySelector('#botao-cadastro')
 var modal = document.querySelector('#meumodal')
-var span = document.querySelector('.fechar')
+var modalSecundario = document.querySelector('#modal-secundario')
+var span = document.querySelectorAll('.fechar')
 var btnEnviar = document.querySelector('#enviar')
-var btnExcluir = document.querySelector('#excluirConta')
+var btnExcluir = document.querySelector('.excluirUsuario')
+var inputs = document.querySelectorAll('.preencher')
+var inputs2 = document.querySelectorAll('.preencher2')
+var inputValues = []
+var btnSalvar = document.querySelector('#salvar')
 
 function adcElemento(nome, email, sobrenome, cidade) {
-    var tabela = document.querySelector('tbody')
+/*    var tabela = document.querySelector('tbody')
     var elementoTr = document.createElement('tr')
     var elementoTd1 = document.createElement('td')
     var elementoTd2 = document.createElement('td')
@@ -13,8 +18,9 @@ function adcElemento(nome, email, sobrenome, cidade) {
     var elementoTd4 = document.createElement('td')
     var elementoTd5 = document.createElement('td')
     var btnExcluirUsuario = document.createElement('button')
+    var btnEditarUsuario = document.createElement('button')
     btnExcluirUsuario.setAttribute('class', 'excluirConta')
-
+    btnEditarUsuario.setAttribute('class', 'editarUsuario')
 
     tabela.appendChild(elementoTr)
     elementoTr.appendChild(elementoTd1)
@@ -23,74 +29,101 @@ function adcElemento(nome, email, sobrenome, cidade) {
     elementoTr.appendChild(elementoTd4)
     elementoTr.appendChild(elementoTd5)
     elementoTd5.appendChild(btnExcluirUsuario)
+    elementoTd5.appendChild(btnEditarUsuario)
     
     elementoTd1.innerHTML = nome
     elementoTd2.innerHTML = sobrenome
     elementoTd3.innerHTML = email
     elementoTd4.innerHTML = cidade
     btnExcluirUsuario.innerHTML = 'excluir'
+    btnEditarUsuario.innerHTML = 'editar'
+
+    
+    */
+   //testando novo codigo
+
+    var tabela = document.querySelector('table')
+    var linha = tabela.insertRow()
+    var celulaNome = linha.insertCell(0)
+    var celulaSobrenome = linha.insertCell(1)
+    var celulaEmail = linha.insertCell(2)
+    var celulaCidade = linha.insertCell(3)
+    var btnEditarUsuario = linha.insertCell(4)
+    
+    celulaNome.innerHTML = nome
+    celulaSobrenome.innerHTML = sobrenome
+    celulaEmail.innerHTML = email
+    celulaCidade.innerHTML = cidade
+    btnEditarUsuario.innerHTML = `<button>Editar</button>`
+
+    //botao-editar-usuario
+    btnEditarUsuario.addEventListener('click', () => {
+       modalSecundario.style.display = "block"
+   })
+
+   //botao-excluir-usuario
+    /*
+    btnExcluir.addEventListener('click', (event) => {
+    var botaoClicado = event.target
+    var linhaPai = botaoClicado.parentNode.parentNode
+    linhaPai.remove()
+    })
+    */
 }
 
-function entrar() {
-    modal.style.display = "block";
+//abrir-modal-cadastro
+function abrirModal() {
+    modal.style.display = 'block'
 }
 
-function fechar() {
-    modal.style.display = "none";
+//botao-fechar
+function botaoFechar() {
+    modal.style.display = "none"
+    modalSecundario.style.display = "none"
 }
 
-function fecharAoRedor(event){
+//fechar-ao-redor-modal
+function fecharAoRedor(event) {
     if(event.target == modal) {
         modal.style.display = "none"
     }
-}
+    if(event.target == modalSecundario) {
+        modalSecundario.style.display = "none"
+    }}
 
-function enviar(){
-    /*
-    var nomeUsuario = document.querySelector('#nome')
-    var sobrenomeUsuario = document.querySelector('#sobrenome')
-    var emailUsuario = document.querySelector('#email')
-    var cidadeUsuario = document.querySelector('#cidade')
+//botao-enviar
+function enviar () {
+    for(var i = 0; i < inputs.length; i++){
+     inputValues.push(inputs[i].value)
+     if(inputs[i].value == '') {
+         console.log('Está vazio!')
+         return;
+     }
+ }
+ modal.style.display = "none"
+ adcElemento(...inputValues)
+ console.log(...inputValues)
+ inputs.forEach(input => input.value = '')
+ }
+//botao-salvar
+ function botaoSalvar() {
+    console.log('clicou no botao salvar')
+    var nome = document.getElementById('nome')
+    console.log(nome)
+ }
 
-    if(!nomeUsuario.value||!sobrenomeUsuario.value||!emailUsuario.value||!cidadeUsuario.value){
-        console.log('esta vazio')
-    }
-    else{
-    fechar()
-    adcElemento(nomeUsuario.value, sobrenomeUsuario.value, emailUsuario.value, cidadeUsuario.value)
-    console.log(nomeUsuario.value, sobrenomeUsuario.value, emailUsuario.value, cidadeUsuario.value)
-    }
-    */
-
-   var inputs = document.querySelectorAll('input')
-   var inputValues = []
-   for(var i = 0; i < inputs.length; i++){
-    inputValues.push(inputs[i].value)
-    if(inputs[i].value == '') {
-        console.log('Está vazio!')
-        return;
-    }
-}
-fechar()
-adcElemento(...inputValues)
-console.log(...inputValues)
-limparForm()
-}
-
-function limparForm() {
-    nome.value=''
-    sobrenome.value=''
-    email.value=''
-    cidade.value=''
-}
-
-function excluirUsuario() {
-    adcElemento().remove()
-}
-
-
-btn.addEventListener('click', entrar)
-span.addEventListener('click', fechar)
+ //eventos
+btnSalvar.addEventListener('click', botaoSalvar)
+btn.addEventListener('click', abrirModal)
 window.addEventListener('click', fecharAoRedor)
 btnEnviar.addEventListener('click', enviar)
-//btnExcluir.addEventListener('click', excluirUsuario)
+span.forEach((item) => {
+    item.addEventListener('click', botaoFechar) 
+})
+//verificar porque o modal secundario nao fecha quando clica no 'X' --funcionando--
+//criar uma funcao para fechar o modal secundario ao redor --funcionando--
+//verificar porque o modal primario nao apaga as informações(nome,sobrenome,email,cidade) que ja foram registradas do usuario --funcionando--
+//aparecer as informacoes do usuario no modal secundario quando clicar em editar
+//editar e salvar quando clicar em editar usuario
+
+//quando clica no botao salvar, ele nao modifica o elemento do form.
