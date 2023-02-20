e413532961ae6dc434a8f6d2baf1cc28ccef2379
Abraão Araujo <96948947+Abraao4raujo@users.noreply.github.com>
2023-02-20 16:22:24 -0300

    deixando o codigo limpo

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+import {abrirModal, fecharModal, enviarModal} from "./modules/modal.js";
 var btn = document.querySelector('#botao-cadastro')
 var modal = document.querySelector('#modalCadastrar')
 var modalSecondary = document.querySelector('#modalEditar')
 var closeBtn = document.querySelectorAll('.fechar')
 var enviarBtn = document.querySelector('#enviar')
-var deleteBtn = document.querySelector('#excluirUsuario')
-var inputs = document.querySelectorAll('.preencher')
 var inputs2 = document.querySelectorAll('.preencher2')
-var inputValues = []
 var btnSalvar = document.querySelector('#salvarUsuario')
 var excluirUsuario = document.querySelector('.btnExcluirUsuario')
 let selecionarLinha;
 
-function adcElemento(nome, sobrenome, email, cidade) {
+export function adcElemento(nome, sobrenome, email, cidade) {
     var tabela = document.querySelector('table')
     var linha = tabela.insertRow()
     var celulaNome = linha.insertCell(0)
@@ -24,7 +22,7 @@ function adcElemento(nome, sobrenome, email, cidade) {
     celulaEmail.setAttribute('id', 'display-email')
     celulaSobrenome.setAttribute('id', 'display-sobrenome')
     celulaCidade.setAttribute('id', 'display-cidade')
-    linha.id = uuidv4()
+    //linha.id = uuidv4()
 
     celulaNome.innerHTML = nome
     celulaSobrenome.innerHTML = sobrenome
@@ -43,42 +41,16 @@ function adcElemento(nome, sobrenome, email, cidade) {
     })
 }
 
-
 function selecionandoLinha(event) {
     selecionarLinha = event.target.parentNode.parentNode
+
+    document.getElementById('user-name').value = selecionarLinha.children[0].innerHTML
+    document.getElementById('user-sobrenome').value = selecionarLinha.children[1].innerHTML
+    document.getElementById('user-email').value = selecionarLinha.children[2].innerHTML
+    document.getElementById('user-cidade').value = selecionarLinha.children[3].innerHTML
 }
 
-//abrir-modal-cadastro
-function abrirModal() {
-    modal.style.display = 'block'
-    modalSecondary.style.display = 'none'
-}
-
-//botao-fechar
-function fecharModal() {
-    modal.style.display = "none"
-    modalSecondary.style.display = "none"
-}
-
-//botao-enviar
-function enviarModal() {
-    for (var i = 0; i < inputs.length; i++) {
-        inputValues.push(inputs[i].value)
-        if (inputs[i].value == '') {
-            console.log('Está vazio!')
-            return;
-        }
-    }
-
-    modal.style.display = "none"
-    colarInformacoesInput()
-    adcElemento(...inputValues)
-    console.log(...inputValues)
-    inputs.forEach(input => input.value = '')
-    inputValues = []
-}
-
-function colarInformacoesInput() {
+export function colarInformacoesInput() {
     var name = document.getElementById('nome').value
     var email = document.getElementById('email').value
     var sobrenome = document.getElementById('sobrenome').value
@@ -91,7 +63,6 @@ function colarInformacoesInput() {
 }
 
 function salvarMudancas() {
-
         for (var i = 0; i < inputs2.length; i++) {
             if (inputs2[i].value == '') {
                 console.log('Está vazio!')
@@ -108,30 +79,21 @@ function salvarMudancas() {
             selecionarLinha.children[3].innerHTML = cidade
         }
         }
-    modalSecondary.style.display = "none"
+    fecharModal()
 }
 
 function excluirUser() {
     selecionarLinha.remove() 
-
-    document.getElementById('nome').value = ""
-    document.getElementById('email').value = ""
-    document.getElementById('sobrenome').value = ""
-    document.getElementById('cidade').value = ""
-
-    modalSecondary.style.display = "none"
+    fecharModal()
 }
 
-function removerTr(id){
-    id.remove()
-}
-
-function uuidv4() {
+/*function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-  }
-  
+}
+*/
+
 //eventos
 btn.addEventListener('click', abrirModal)
 enviarBtn.addEventListener('click', enviarModal)
